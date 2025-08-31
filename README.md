7bc51303a38894a8498cb2f46fa6a24999a231f4
mmedina <maurimedina89@gmail.com>
2025-08-30 23:48:41 -0300

    Use google calendar api to retrieve holidays

--- README.md
+++ README.md
@@ -1,23 +1,26 @@
 # Feriabotpy ![Twitter Follow](https://img.shields.io/twitter/follow/feriabotpy?style=social)
 
-Feriabotpy is a Twitter bot that tweets every day how many days are left to next holiday in Paraguay.</br>
+Feriabotpy is a Twitter bot that tweets every day how many days are left until the next holiday in Paraguay.
+
+[View Paraguay Holidays Calendar](https://calendar.google.com/calendar/embed?src=07c06218c7964275b5ae1a25565874a0e321fa4d2dc096a475373b7d73f56743%40group.calendar.google.com&ctz=America%2FAsuncion)
 
 ## Installation
 
 ```bash
 git clone https://github.com/mauri-medina/feriabotpy.git
+cd feriabotpy
 pip install -r requirements.txt
 ```
-Copy file `.env.example` into a new file `env.py` and add twitter credentials
+Copy the .env.example file to .env and fill in your Twitter and Google credentials
 
-Run `main.py`
-
-## TO DO
-This is a work in progress, the final idea is to have an API where everybody could retrieve the holiday from a date range
-
+Run the bot with
+```bash
+python main.py
+```
 
 ## Contributing
 Pull requests are welcome.
 
 ## License
 [MIT](https://choosealicense.com/licenses/mit/)
+
--- main.py
+++ main.py
@@ -1,39 +1,78 @@
 import datetime
-import locale
 import os
 
+import locale
 import tweepy
 from dotenv import load_dotenv
+import requests
+from datetime import date, datetime, timedelta
 
-import db
-from models import CelebrationDate
+load_dotenv()
+# google
+calendar_id = os.getenv("GOOGLE_CALENDAR_ID")
+google_api_key = os.getenv("GOOGLE_API_KEY")
+# twitter
+twitter_bearer_token = os.getenv("BEARER_TOKEN")
+twitter_api_key = os.getenv("API_KEY")
+twitter_api_secret_key = os.getenv("API_SECRET_KEY")
+twitter_api_access_token = os.getenv("ACCESS_TOKEN")
+twitter_api_secret_access_token = os.getenv("SECRET_ACCESS_TOKEN")
 
 
-def run():
-    today = datetime.date.today()
-    next_holiday = CelebrationDate.get_next_from_date(today)
-    if next_holiday.date == today:
-        message = f"{next_holiday.holiday.message}"
+class Holiday:
+    def __init__(self, name: str, message: str, date: datetime):
+        self.name = name
+        self.message = message
+        self.date = date
+
+
+def main():
+    next_holiday = get_next_holiday()
+    if next_holiday:
+        today = datetime.today().date()
+
+        if next_holiday.date.date() == today:
+            tweet(next_holiday.message)
+            tomorrow = today + timedelta(days=1)
+            next_holiday = get_next_holiday(from_date=datetime.combine(tomorrow, datetime.min.time()))
+
+        days_to_next_holiday = (next_holiday.date.date() - today).days
+        message = ""
+
+        if days_to_next_holiday == 1:
+            message = "Mañana es Feriado! No se olviden de apagar sus alarmas\n"
+        else:
+            message = f"Faltan {days_to_next_holiday} días para el siguiente feriado\n"
+
+        message += f"Feriado: {next_holiday.name}\n"
+        message += """Fecha: {0} {1} de {2}""".format(
+            next_holiday.date.strftime("%A"),  # name of weekday
+            next_holiday.date.strftime("%e"),  # day of month without leading zero
+            next_holiday.date.strftime("%B"),  # month name
+        )
+
         tweet(message)
 
-        tomorrow = today + datetime.timedelta(days=1)
-        next_holiday = CelebrationDate.get_next_from_date(tomorrow)
 
-    days_to_holiday = (next_holiday.date - today).days
-    message = ""
-    if days_to_holiday == 1:
-        message = "Mañana es Feriado! No se olviden de apagar sus alarmas\n"
-    else:
-        message = f"Faltan {days_to_holiday} días para el siguiente feriado\n"
-
-    message += f"Feriado: {next_holiday.holiday.name}\n"
-    message += """Fecha: {0} {1} de {2}""".format(
-        next_holiday.date.strftime("%A"),  # name of weekday
-        next_holiday.date.strftime("%e"),  # day of month without leading zero
-        next_holiday.date.strftime("%B"),  # month name
-    )
-
-    tweet(message)
+def get_next_holiday(from_date: date = datetime.now()):
+    params = {
+        "key": google_api_key,
+        "timeMin": from_date.astimezone().isoformat(),
+        "maxResults": 1,
+        "orderBy": "startTime",
+        "singleEvents": "true",
+    }
+    holiday = None
+    response = requests.get(f"https://www.googleapis.com/calendar/v3/calendars/{calendar_id}/events", params=params)
+    items = response.json().get("items", [])
+    if items:
+        item = items[0]
+        holiday = Holiday(
+            name=item.get("summary").strip(),
+            message=item.get("description", "").strip(),
+            date=datetime.fromisoformat(item.get("start").get("date")),
+        )
+    return holiday
 
 
 def tweet(message: str) -> None:
@@ -42,11 +81,11 @@ def tweet(message: str) -> None:
         return
 
     client = tweepy.Client(
-        os.getenv("BEARER_TOKEN"),
-        os.getenv("API_KEY"),
-        os.getenv("API_SECRET_KEY"),
-        os.getenv("ACCESS_TOKEN"),
-        os.getenv("SECRET_ACCESS_TOKEN"),
+        twitter_bearer_token,
+        twitter_api_key,
+        twitter_api_secret_key,
+        twitter_api_access_token,
+        twitter_api_secret_access_token,
     )
 
     client.create_tweet(text=message)
@@ -55,7 +94,4 @@ def tweet(message: str) -> None:
 if __name__ == "__main__":
     # we want month and days names in Spanish
     locale.setlocale(locale.LC_ALL, "es_ES.UTF-8")
-    load_dotenv()
-
-    db.Base.metadata.create_all(db.engine)
-    run()
+    main()
