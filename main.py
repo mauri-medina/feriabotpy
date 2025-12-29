aa399cf880e343ad5e63f6090247f21d2686064a
Mauricio Medina <maurimedina89@gmail.com>
2025-12-29 01:10:02 -0300

    more debug

--- main.py
+++ main.py
@@ -81,11 +81,10 @@ def tweet(message: str) -> None:
         print(message)
         return
 
-    print("twitter_bearer_token: " + twitter_bearer_token)
-    print("twitter_api_key: " + twitter_api_key)
-    print("twitter_api_secret_key " + twitter_api_secret_key)
-    print("twitter_api_access_token: " + twitter_api_access_token)
-    print("twitter_api_secret_access_token: " + twitter_api_secret_access_token)
+    print(f"twitter_api_key: |{twitter_api_key}|" + )
+    print(f"twitter_api_secret_key : |{twitter_api_secret_key}|")
+    print(f"twitter_api_access_token: : |{twitter_api_access_token}|")
+    print(f"twitter_api_secret_access_token: : |{twitter_api_secret_access_token}|")
 
     client = tweepy.Client(
     consumer_key=twitter_api_key, 
